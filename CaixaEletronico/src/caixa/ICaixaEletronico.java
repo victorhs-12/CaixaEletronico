c969adb38c72e3f18e48115a80a352b84ef6d761
Victor Hugo <vixs1212@gmail.com>
2026-04-18 19:14:10 -0300

    chamando métodos na classe da interface

--- CaixaEletronico/src/caixa/ICaixaEletronico.java
+++ CaixaEletronico/src/caixa/ICaixaEletronico.java
@@ -1,5 +1,14 @@
 package caixa;
 
 public interface ICaixaEletronico {
-
+	
+	public String pegaValorTotalDisponivel();
+	
+	public String sacar(Integer valor);
+	
+	public String pegaRelatorioCedulas();
+	
+	public String reposicaoCedulas(Integer cedula, Integer quantidade);
+	
+	public String armazenaCotaMinima(Integer minimo);
 }
